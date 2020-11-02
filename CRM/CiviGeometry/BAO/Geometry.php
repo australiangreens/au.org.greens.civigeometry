6eb5e8e2a83cd598073d55e07e3caef9edd53d82
Anthony <anthony@unfinishedteleporter.com>
2020-11-02 17:14:26 +0800

    Fix incorrect exception class name

--- CRM/CiviGeometry/BAO/Geometry.php
+++ CRM/CiviGeometry/BAO/Geometry.php
@@ -593,7 +593,7 @@ class CRM_CiviGeometry_BAO_Geometry extends CRM_CiviGeometry_DAO_Geometry {
         $offset += $params['batch_size'];
       }
       else {
-        throw new CRM_Core_ExecptionException(E::ts("No candidate addresses, but batches still remaining to process. That doesn't make any sense."));
+        throw new CRM_Core_Exception(E::ts("No candidate addresses, but batches still remaining to process. That doesn't make any sense."));
       }
     }
   }

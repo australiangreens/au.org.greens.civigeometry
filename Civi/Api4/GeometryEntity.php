4928ac2d9403a5492d0f5019d3a635cee24f75eb
John Twyman <john.twyman@greens.org.au>
2023-07-27 18:03:35 +1000

    Add Civi API4 GeometryEntity entity

---
+++ Civi/Api4/GeometryEntity.php
@@ -0,0 +1,13 @@
+<?php
+namespace Civi\Api4;
+
+/**
+ * GeometryEntity entity.
+ *
+ * Provided by the CiviGeometry extension.
+ *
+ * @package Civi\Api4
+ */
+class GeometryEntity extends Generic\DAOEntity {
+
+}
